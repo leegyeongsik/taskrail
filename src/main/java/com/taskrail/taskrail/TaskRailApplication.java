da2bc73034d5b65867fe7790086ff2e923c6ea3f
leegyeongsik <gyeongsik1012@gmail.com>
2023-08-07 11:57:34 +0900

    init commit

---
+++ src/main/java/com/taskrail/taskrail/TaskRailApplication.java
@@ -0,0 +1,13 @@
+package com.taskrail.taskrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskRailApplication.class, args);
+    }
+
+}
