2c5e633b1fb5ec89fa474062cf2c5d4f31edaf9b
chae0 <limchae098@gmail.com>
2023-08-08 14:51:23 +0900

    fix : users table id -> IDENTITY

--- src/main/java/com/taskrail/entity/User.java
+++ src/main/java/com/taskrail/entity/User.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
